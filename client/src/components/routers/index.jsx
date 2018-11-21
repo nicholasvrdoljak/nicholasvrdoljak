c5553fc0a5064fadcff583235189b9c43995c18d
nicholasvrdoljak <admin@nicholasvrdoljak.com>
2018-11-21 14:28:35 -0800

    Secured area

--- client/src/components/routers/index.jsx
+++ client/src/components/routers/index.jsx
@@ -6,6 +6,8 @@ import Contact from '../contact/index.jsx';
 import Play from '../play/index.jsx';
 import Photography from '../photography/index.jsx';
 import Projects from '../projects/index.jsx';
+import Secure from '../secure/index.jsx';
+import One from '../secure/components/one.jsx';
 
 
 const Main = () => (
@@ -17,6 +19,8 @@ const Main = () => (
             <Route exact path='/photography' render={(props) => <Home {...props} prop={'two'}/>}/>
             <Route exact path='/play' render={(props) => <Home {...props} prop={'three'}/>}/>
             <Route exact path='/contact' render={(props) => <Home {...props} prop={'four'}/>}/>
+            <Route exact path='/secure' render={() => <Secure/>}/>
+            <Route exact path='/secure/one' render={() => <One/>}/>
         </Switch>
     </main>
 );
---
+++ client/src/components/secure/components/one.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export default class One extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <div className='contact'>
+                <div className='contactTitle'>
+                    <h1>Secure Area One</h1>
+                </div>
+
+
+                <div className='contactItem'>
+                </div>
+            </div>
+
+        )
+    }
+}
