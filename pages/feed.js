e9683db391cc75a8048871e52233a022be16a79d
nicholasvrdoljak <admin@nicholasvrdoljak.com>
2021-06-02 10:34:06 -0600

    Remove items

--- pages/feed.js
+++ pages/feed.js
@@ -24,35 +24,35 @@ const posts = [
 		type: 'youtube', 
 		title: 'Pokemon Cards',
 		link: 'wuBABzFi3r4', 
-		date: 'May 25, 2021'
+		// date: 'May 25, 2021'
 	},
 	{
 		type: 'youtube',
 		title: "Suburbs that don't Suck",
 		link: 'MWsGBRdK2N0',
-		date: 'May 24, 2021'
+		// date: 'May 24, 2021'
 	},
 	{
 		type: 'youtube',
 		title: 'The Insane Logistics of Shutting Down the Cruise Industry',
 		link: 'N4dOCfWlgBw',
-		date: 'May 24, 2021'
+		// date: 'May 24, 2021'
 	},
-	{
-		type: 'quote',
-		title: 'C.S. Lewis', 
-		excerpt: "Of all tyrannies, a tyranny sincerely exercised for the good of its victims may be the most oppressive. It would be better to live under robber barons than under omnipotent moral busybodies. The robber baron's cruelty may sometimes sleep, his cupidity may at some point be satiated; but those who torment us for our own good will torment us without end for they do so with the approval of their own conscience. They may be more likely to go to Heaven yet at the same time likelier to make a Hell of earth. This very kindness stings with intolerable insult. To be 'cured' against one's will and cured of states which we may not regard as disease is to be put on a level of those who have not yet reached the age of reason or those who never will; to be classed with infants, imbeciles, and domestic animals",
-		image: "",
-		date: "May 22, 2021"
-	},
-	{
-		type: 'article',
-		title: "I can tolerate anything except the outgroup",
-		excerpt: "Slate Star Codex",
-		href: "https://web.archive.org/web/20210105161328/https://slatestarcodex.com/2014/09/30/i-can-tolerate-anything-except-the-outgroup/",
-		image: "/images/outgroup.jpg",
-		date: "January 27, 2021"
-	}, 
+	// {
+	// 	type: 'quote',
+	// 	title: 'C.S. Lewis', 
+	// 	excerpt: "Of all tyrannies, a tyranny sincerely exercised for the good of its victims may be the most oppressive. It would be better to live under robber barons than under omnipotent moral busybodies. The robber baron's cruelty may sometimes sleep, his cupidity may at some point be satiated; but those who torment us for our own good will torment us without end for they do so with the approval of their own conscience. They may be more likely to go to Heaven yet at the same time likelier to make a Hell of earth. This very kindness stings with intolerable insult. To be 'cured' against one's will and cured of states which we may not regard as disease is to be put on a level of those who have not yet reached the age of reason or those who never will; to be classed with infants, imbeciles, and domestic animals",
+	// 	image: "",
+	// 	date: "May 22, 2021"
+	// },
+	// {
+	// 	type: 'article',
+	// 	title: "I can tolerate anything except the outgroup",
+	// 	excerpt: "Slate Star Codex",
+	// 	href: "https://web.archive.org/web/20210105161328/https://slatestarcodex.com/2014/09/30/i-can-tolerate-anything-except-the-outgroup/",
+	// 	image: "/images/outgroup.jpg",
+	// 	date: "January 27, 2021"
+	// }, 
 ];
 
 const useStyles = makeStyles((theme) => ({
