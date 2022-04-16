5e50163ec74ac37eba05feaa9fd87399e0e10f8f
nicholasvrdoljak <vrdoljaknicholas@gmail.com>
2022-04-16 14:45:20 -0600

    Add back drawer functionality

--- components/header.js
+++ components/header.js
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
-// import Divider from '@mui/material/Divider';
-// import Drawer from '@mui/material/Drawer';
-// import Hidden from '@mui/material/Hidden';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import Hidden from '@mui/material/Hidden';
 import IconButton from '@mui/material/IconButton';
-// import List from '@mui/material/List';
-// import ListItem from '@mui/material/ListItem';
-// import ListItemText from '@mui/material/ListItemText';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
@@ -82,12 +82,16 @@ const useStyles = makeStyles((theme) => ({
 	},
 	content: {
 		flexGrow: 1,
+		[theme.breakpoints.up('sm')]: {
+			width: `calc(100% - ${drawerWidth}px)`,
+			marginLeft: `-${drawerWidth}px`,
+		},
 	}
 }));
 
 function Header(props) {
 	const { Component, pageProps } = props;
-
+	const { window } = props;
 	const theme = useTheme();
 	const classes = useStyles(theme);
 	const [mobileOpen, setMobileOpen] = React.useState(false);
@@ -113,7 +117,24 @@ function Header(props) {
 			);
 		});
 	};
+	const drawer = (
+		<div>
+			<div className={classes.toolbar} />
+			<ListItem button onClick={handleDrawerToggle}><Link href="/" passHref><ListItemText>Home</ListItemText></Link></ListItem>
+			<Divider />
+			<List>
+				{headersData.map((item, index) => (
+					<ListItem button onClick={handleDrawerToggle} key={item.label}>
+						<Link href={item.href} passHref>
+							<ListItemText>{item.label}</ListItemText>
+						</Link>
+					</ListItem>
+				))}
+			</List>
+		</div>
+	);
 
+	const container = window !== undefined ? () => window().document.body : undefined;
 	return (
         <div className={classes.root}>
 			<AppBar position="fixed" className={classes.appBar}>
@@ -124,12 +145,12 @@ function Header(props) {
 						</Typography>
 					</Link>
 					<IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        edge="start"
-                        onClick={handleDrawerToggle}
-                        className={classes.menuButton}
-                        size="large">
+	          color="inherit"
+	          aria-label="open drawer"
+	          edge="start"
+	          onClick={handleDrawerToggle}
+	          className={classes.menuButton}
+	          size="large">
 					<MenuIcon />
 				</IconButton>
 				<div className={classes.menuLinksContainer}>
@@ -138,7 +159,25 @@ function Header(props) {
 				</Toolbar>
 
 			</AppBar>
-
+			<nav className={classes.drawer} aria-label="mailbox folders">
+				<Hidden smUp implementation="css">
+				<Drawer
+					container={container}
+					variant="temporary"
+					anchor={'right'}
+					open={mobileOpen}
+					onClose={handleDrawerToggle}
+					classes={{
+					paper: classes.drawerPaper,
+					}}
+					ModalProps={{
+					keepMounted: true, // Better open performance on mobile.
+					}}
+				>
+					{drawer}
+				</Drawer>
+				</Hidden>
+			</nav>
 			<main className={classes.content}>
 				<div className={classes.toolbar} />
 				<Component {...pageProps}/>
