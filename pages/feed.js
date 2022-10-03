ec9e2fe43051dfb13bad3c9954a41b8c269c0a69
Nick Vrdoljak <34222780+nicholasvrdoljak@users.noreply.github.com>
2022-10-03 12:30:53 -0600

    Update feed.js

--- pages/feed.js
+++ pages/feed.js
@@ -10,6 +10,26 @@ import Link from 'next/link';
 import makeStyles from '@mui/styles/makeStyles';
 
 const posts = [
+	{
+		type: 'quote',
+		title: 'Percy Shelley, "Ozymandias"',
+		excerpt: `I met a traveller from an antique land\n
+		Who said: Two vast and trunkless legs of stone\n
+		Stand in the desart.[d] Near them, on the sand,\n
+		Half sunk, a shattered visage lies, whose frown,\n
+		And wrinkled lip, and sneer of cold command,\n
+		Tell that its sculptor well those passions read\n
+		Which yet survive, stamped on these lifeless things,\n
+		The hand that mocked them and the heart that fed:\n
+		And on the pedestal these words appear:\n
+		"My name is Ozymandias, king of kings:\n
+		Look on my works, ye Mighty, and despair!"\n
+		Nothing beside remains. Round the decay\n
+		Of that colossal wreck, boundless and bare\n
+		The lone and level sands stretch far away.`,
+		date: 'October 3, 2022',
+		image: '',
+	},
 // 	{
 // 		type: 'youtube',
 // 		title: 'Something',
